821153fde82fe06c0a17d4604e03af1e0dfbc724
yurasicus <32620744+yurasicus@users.noreply.github.com>
2022-05-19 02:13:10 +0500

    fixes

--- Project 0/game_v3.py
+++ Project 0/game_v3.py
@@ -1,3 +1,26 @@
+import numpy as np
+
+def score_game(predict_func) -> float:
+    """ Makes 1000 runs of given input function.
+        Returns average attempts count made until win.
+        
+    Args:
+        predict_func (_type_): tested function
+
+    Returns:
+        float: average attempts count until win
+    """
+    
+    total_runs = 1000                   # number of runs
+    total_trials = 0
+    
+    for _ in range(total_runs):
+        test_number = np.random.randint(1, 101)
+        print(f'trying {test_number} number, run {_}')
+        total_trials += predict_func(test_number)  # total attempt count in run
+    
+    # average attempt count in 1000 runs:
+    return total_trials / total_runs  
 
 
 def smart_predict(number:int=1) -> int:
@@ -14,15 +37,19 @@ def smart_predict(number:int=1) -> int:
     
     trial_count = 0
     left_bound = 0
-    right_bound = 100
+    right_bound = 100 + 1
     
     while True:
         trial_number = (left_bound + right_bound) // 2
+        print(left_bound, trial_number, right_bound)
         trial_count += 1
         if trial_number == number:
             return trial_count
-        elif trial_number < number:
-            right_bound = trial_number
         elif trial_number > number:
+            right_bound = trial_number
+        elif trial_number < number:
             left_bound = trial_number
 
+
+if __name__ == '__main__':
+    print(score_game(smart_predict))
