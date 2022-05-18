4e9a9eb7e5cd3be13e239646958344d4b30e0238
yurasicus <32620744+yurasicus@users.noreply.github.com>
2022-05-19 01:27:03 +0500

    minor formatting, translation

--- Project 0/game_v2.py
+++ Project 0/game_v2.py
@@ -7,7 +7,7 @@ def random_predict(number:int=1) -> int:
         number (int, optional): Given proposed number. Defaults to 1.
 
     Returns:
-        int: number of tries until win
+        int: number of attempts until win
     """
 
     count = 0
@@ -21,25 +21,27 @@ def random_predict(number:int=1) -> int:
 
 def score_game(predict_func) -> float:
     """ Makes 1000 runs of given input function.
-        Returns average try count made until win.
+        Returns average attempts count made until win.
         
     Args:
         predict_func (_type_): tested function
 
     Returns:
-        float: average try count until win
+        float: average attempts count until win
     """
     
     total_runs = 1000                   # number of runs
     total_trials = 0
     
     for _ in range(total_runs):
-        total_trials += predict_func()  # total try count in run
+        total_trials += predict_func()  # total attempt count in run
     
-    return total_trials / total_runs  # average try count in 1000 runs
+    # average attempt count in 1000 runs:
+    return total_trials / total_runs  
 
 
-# print(f'Average try count to win: {random_predict()}')
+
+# print(f'Average attempt count to win: {random_predict()}')
 
 if __name__ == '__main__':
     print(score_game(random_predict))
